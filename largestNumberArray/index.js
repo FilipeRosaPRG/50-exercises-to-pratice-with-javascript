995c16bd058f6a4e1a91d29af82f568bcdc6cbb6
Filipe Rosa <filipealves2621@gmail.com>
2023-03-08 09:51:30 -0300

    Get the largest number
    en-us - Write a function that returns the largest number from an array of numbers.
    
    pt-br - Escreva uma função que retorne o maior número de um array de números.

---
+++ largestNumberArray/index.js
@@ -0,0 +1,13 @@
+function getLargestNumber() {
+    var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    var largestNumber = numbers[0];
+
+    for (var i = 0; i < numbers.length; i++) {
+        if (numbers[i] > largestNumber) {
+            largestNumber = numbers[i];
+        }
+    }
+    console.log(largestNumber);
+}
+
+getLargestNumber();
